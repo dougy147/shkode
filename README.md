b3aed0378541992990a071079dcfd0a08826a110
dougy147 <16362303+dougy147@users.noreply.github.com>
2025-03-15 12:31:36 +0100

    Allow live-coding via https://shkode.nopub.club

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 Serve your current coding buffer to a live-updated webpage.
+
+```console
+# server side
+$ git clone https://github.com/dougy147/shkode
+$ cd ./shkode
+$ docker compose up -d
+# streamer side
+$ ./shkode file.txt
+[INFO] Serving "file.txt" to "z747SLYJ2rDRyLe".
+[INFO] Access live code here: https://shkode.nopub.club/@/z747SLYJ2rDRyLe/
+```
---
+++ docker/data/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <!--<meta charset="UTF-8">-->
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>shkode</title>
+    <!--<link rel="icon" href="images/shkode.png" type="image/icon type">-->
+</head>
+
+<body>
+    <!--
+    <noscript>
+      <style>html{display:none;}</style>
+      <meta http-equiv="refresh" content="0.0;url=./index.html">
+    </noscript>
+    -->
+    <?php
+        //if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        //    $data = $_POST['data'];
+        //    echo 'Received data: ' . htmlspecialchars($data);
+	//};
+	if ($_SERVER["REQUEST_METHOD"] == "GET") {
+	    if (isset($_GET['page']) and isset($_GET['code'])) {
+		//ob_start();
+		$code = $_GET['code'];
+		$code = base64_decode($code);
+		//$code = mb_convert_encoding($code, 'UTF-8', 'auto');
+		$page = $_GET['page'];
+		if (! is_dir("./@/$page")) {
+		    mkdir("./@/$page",0755,true);
+		};
+		file_put_contents("./@/$page/code.txt", $code);
+		if (! file_exists("./@/$page/index.html")) {
+		    $template = file_get_contents('template.html');
+		    $template = html_entity_decode($template);
+		    //$template = str_replace("SRC", "./code.txt", $template);
+		    file_put_contents("./@/$page/index.html", $template);
+		};
+		//ob_end_flush();
+		////echo '<form action="post_handler.php" method="post">
+		////    <input type="hidden" name="data" value="some_value">
+		////    <input type="submit" value="Submit">
+		////    </form>';
+	    };
+	};
+    ?>
+</body>
+</html>
