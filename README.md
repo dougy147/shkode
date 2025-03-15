8876c39e19b48f055579703c5c3886805fb80d89
dougy147 <16362303+dougy147@users.noreply.github.com>
2025-03-15 18:57:42 +0100

    (v0.0.1-beta) Fix arguments + change webpage.

--- README.md
+++ README.md
@@ -1,4 +1,35 @@
-Serve your current coding buffer to a live-updated webpage.
+Stream your code to a live-updated webpage (also see [https://shkode.nopub.club](https://shkode.nopub.club)).
+
+# Use with Perl script
+
+Download `shkode` Perl script, then:
+
+```console
+$ chmod +x ./shkode
+$ ./shkode /path/to/your/code.py
+[INFO] Serving "code.py" to "zWPeQt4wgj6S4oj".
+[INFO] Access live code here: https://shkode.nopub.club/@/zWPeQt4wgj6S4oj/
+```
+
+Go to indicated URL to see your code live.
+
+# Use from Bash
+
+```bash
+file=/path/to/your/code.rs
+ur_pagename=MY_OWN_PAGE # make this unique
+while :; do
+    encoded=$(base64 $file -w 0)
+    curl -s https://shkode.nopub.club -G --data-urlencode "code=$encoded" -d "page=$ur_pagename"
+    sleep 5
+done
+```
+
+Go to `https://shkode.nopub.club/@/MY_OWN_PAGE` to see your code live.
+
+# For nerds
+
+Some details on how to deploy your own `shkode` server.
 
 ```console
 # server side
@@ -11,16 +42,7 @@ $ ./shkode file.txt
 [INFO] Access live code here: https://shkode.nopub.club/@/z747SLYJ2rDRyLe/
 ```
 
-# Configure nginx server side
-
-```console
-# /etc/nginx/nginx.conf
-http {
-    ...
-    large_client_header_buffers 10 128k;
-    ...
-}
-```
+Doc to come (or not...).
 
 # TODO
 
--- docker/data/index.php
+++ docker/data/index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="style.css">
     <title>shkode</title>
-    <link rel="icon" href="" type="image/icon type">
+    <link rel="icon" href="./favicon.png" type="image/icon type">
     <!-- reduce flashbang for some browsers -->
     <style>body { display: none; }</style>
     <script>window.onload = function() { document.body.style.display = 'block'; };</script>
@@ -13,10 +13,10 @@
 
 <body>
     <center>
-	<h1>shkode</h1>
+	<h1><span style="color:grey">#!/usr/bin/env perl</span> <span style="color:yellow">shkode</span></h1>
 
 	<div>
-	    <p>Serve your current coding buffer to a live-updated webpage.</p>
+	    <p>Stream your code to a live-updated webpage.</p>
 	</div>
     </center>
 
@@ -54,7 +54,19 @@
     	        /*$template = str_replace("SRC", "./code.html", $template);*/
     	        file_put_contents("./@/$page/index.html", $template);
     	    };
-    	};
+	};
     ?>
+
+<div class="demo">
+<pre><code><span style="color:grey">#!/usr/bin/env bash</span>
+<span style="color:cyan">file</span><span style="color:white">=/path/to/code.py</span>
+<span style="color:cyan">ur_page</span><span style="color:white">=my_own_page</span> <span style="color:grey"># make this unique</span>
+<span style="color:red">while :</span><span style="color:orange">; </span><span style="color:red">do</span>
+    <span style="color:cyan">encoded</span><span style="color:white">=</span><span style="color:lightgreen">$(</span><span style="color:orange">base64</span> <span style="color:lightgreen">$file</span> <span style="color:orange">-w</span> <span style="color:magenta">0</span><span style="color:lightgreen">)</span>
+    <span style="color:white">curl</span> <span style="color:red">-s</span> <a style="color:white" href="https://shkode.nopub.club">https://shkode.nopub.club</a> <span style="color:red">-G</span> <span style="color:orange">--data-urlencode</span> <span style="color:orange">"</span><span style="color:yellow">code=</span><span style="color:lightgreen">$encoded</span><span style="color:orange">"</span> <span style="color:red">-d</span> <span style="color:orange">"</span><span style="color:yellow">page=</span><span style="color:lightgreen">$ur_page</span><span style="color:orange">"
+    <span style="color:red">sleep</span> <span style="color:magenta">5</span>
+<span style="color:red">done</span></pre></code>
+</div>
+
 </body>
 </html>
